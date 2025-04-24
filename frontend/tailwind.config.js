19971279d3b0dd0e12e8c0336b417000ecb75008
Luke Henderson <personal@lukeh.us>
2025-04-24 02:51:47 -0700

    created react app, initialized tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./publix/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
