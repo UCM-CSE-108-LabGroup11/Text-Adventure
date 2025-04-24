05672dd7db73adc6b1f3ef11fbe770d516ca5460
Luke Henderson <personal@lukeh.us>
2025-04-24 05:02:34 -0700

    started work on homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+    const [data, setData] = useState([{}]);
+
+    useEffect (() => {
+        fetch ("/test").then (
+            res => res.json ()
+        ).then (
+            data => {
+                setData (data);
+                console.log(data);
+            }
+        )
+    }, []);
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div>
+            {(typeof data.test === "undefined") ? (
+                <p>Loading...</p>
+            ) : (
+                data.test.map ((test: string, i: number) => (
+                    <p key={i}>{test}</p>
+                ))
+            )}
         </div>
     );
 }
